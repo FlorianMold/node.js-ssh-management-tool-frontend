12e72b9d7e30a585ff67614744bee6a1a07eadc0
Florian Mold <florian.mold@live.at>
2020-05-23 12:52:37 +0200

    Document code in client-api and request-service.

--- src/service/api/ClientApi.ts
+++ src/service/api/ClientApi.ts
@@ -1,9 +1,10 @@
 import * as RequestService from "../request/RequestService";
-import {Client} from "../../model/Client";
-import {HttpResponse} from "../request/http/HttpResponse";
 
 export class ClientApi {
 
-static fetchClients = async () => (await RequestService.get("clients")).payload;
+    /**
+     * Fetches all clients.
+     */
+    static fetchClients = async () => (await RequestService.get("clients")).payload;
 
 }
--- src/service/request/RequestService.ts
+++ src/service/request/RequestService.ts
@@ -2,22 +2,51 @@ import {requestInstance} from "./request";
 import {HttpRequestPayload} from "./http/HttpRequestPayload";
 import {HttpResponse} from "./http/HttpResponse";
 
+/**
+ * Performs a get-request.
+ *
+ * @param resource resource to be requested.
+ * @param params request-params
+ */
 export const get = async (resource: string, params?: object): Promise<HttpResponse> => {
     return new HttpResponse(await requestInstance.get(resource, params));
 }
 
+/**
+ * Performs a post-request.
+ *
+ * @param resource resource that is requested.
+ * @param payload payload of the request.
+ */
 export const post = async (resource: string, payload: HttpRequestPayload): Promise<HttpResponse> => {
     return new HttpResponse(await requestInstance.post(resource, payload));
 }
 
+/**
+ * Performs a put-request.
+ *
+ * @param resource resource that is requested.
+ * @param payload payload of the request.
+ */
 export const put = async (resource: string, payload: HttpRequestPayload): Promise<HttpResponse> => {
     return new HttpResponse(await requestInstance.put(resource, payload));
 }
 
+/**
+ * Performs a patch-request.
+ *
+ * @param resource resource that is requested.
+ * @param payload payload of the request.
+ */
 export const patch = async (resource: string, payload: HttpRequestPayload): Promise<HttpResponse> => {
     return new HttpResponse(await requestInstance.patch(resource, payload));
 }
 
+/**
+ * Performs a delete request.
+ *
+ * @param resource resource to be deleted.
+ */
 export const remove = async (resource: string): Promise<HttpResponse> => {
     return new HttpResponse(await requestInstance.delete(resource));
 }
